1862f19fa707bc99f905a56c96d24cd5b850b804
Lucas Ody <lucas_ody@hotmail.com>
2021-05-02 16:04:15 -0300

    Endpoint /clientes/{id}

---
+++ src/main/java/com/lucasody/cursomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.lucasody.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lucasody.cursomc.domain.Cliente;
+import com.lucasody.cursomc.repositories.ClienteRepository;
+import com.lucasody.cursomc.services.exepctions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired //puxa a dependencia automaticamente para categoriarepository
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		 "Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName())); 
+
+	}
+}
