47f8833840e731d6ad6eaddd198b5caa67acd4b7
Lucas Ody <lucas_ody@hotmail.com>
2021-05-02 14:27:33 -0300

    Cliente, TipoCliente, Telefone, Endereco

---
+++ src/main/java/com/lucasody/cursomc/repositories/EnderecoRepository.java
@@ -0,0 +1,13 @@
+package com.lucasody.cursomc.repositories;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lucasody.cursomc.domain.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+
+}
