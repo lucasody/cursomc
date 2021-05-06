458da9fa4389febb8068030f0bb77b259fe63caf
Lucas Ody <lucas_ody@hotmail.com>
2021-05-06 17:26:53 -0300

    Testando todas categorias com DTO

--- src/main/java/com/lucasody/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/lucasody/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.lucasody.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.lucasody.cursomc.domain.Categoria;
+import com.lucasody.cursomc.dto.CategoriaDTO;
 import com.lucasody.cursomc.services.CategoriaService;
 
 @RestController
@@ -49,4 +52,14 @@ public class CategoriaResource {
 		service.delete(id);
 		return ResponseEntity.noContent().build();			
 	}
+	
+	@RequestMapping(method=RequestMethod.GET) //Solicita categoria/id
+	public ResponseEntity<List<CategoriaDTO>> findAll(){
+		
+		List<Categoria> list = service.findAll(); //busca lista de categorias
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);				
+	}
 }
+
+
--- src/main/java/com/lucasody/cursomc/services/CategoriaService.java
+++ src/main/java/com/lucasody/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.lucasody.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,6 +42,10 @@ public class CategoriaService {
 		catch (DataIntegrityViolationException e){ //tentar deletar categoria com produtos
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
 		}
+	}
+	
+	public List<Categoria> findAll(){ //retorna lista com todas categorias
+		return repo.findAll();
 	}
 }
 
